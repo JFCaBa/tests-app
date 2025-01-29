8c6987f29b0c8ae97d6bdda79ebf401378384bd8
Jose Catala <jfca68@gmail.com>
2025-01-29 12:30:47 +0300

    Allow domains in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,13 +10,15 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,
+    host: true, // Allows access via LAN & IPv6
     port: 5173,
+    strictPort: true, // Ensures Vite doesn't switch ports if 5173 is occupied
     proxy: {
       "/api": {
         target: "http://localhost:1999",
         changeOrigin: true,
       },
     },
+    allowedHosts: ["testmyrussian.com", "www.testmyrussian.com"], // Allow domain
   },
 });
