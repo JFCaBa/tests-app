650018e36b62509075247401a5eb265fd3f97272
Jose Catala <jfca68@gmail.com>
2025-02-02 22:41:29 +0300

    fixed export name

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,9 +1,9 @@
 import User from "./User.js";
 import Question from "./Question.js";
-import Chat from "./Chat.js";
+import ChatMessage from "./Chat.js";
 import Cache from "./Cache.js";
 
-export { User, Question, Chat, Cache };
+export { User, Question, ChatMessage, Cache };
 
 // This allows both:
 // import { User, Question } from '../models'
