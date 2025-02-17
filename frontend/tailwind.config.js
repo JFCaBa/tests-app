be5d2c9d6d2e60dd00cd8acd30e2177a0c923ae8
Jose Catala <jfca68@gmail.com>
2025-02-17 13:23:26 +0300

    1

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./index.html",
+    "./public/index.html",
     "./pages/**/*.{js,jsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx}",
